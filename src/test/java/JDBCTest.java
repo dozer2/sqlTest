fb7f9fbdec9254295ec9090dcbf99b9a569d96f0
maxim <ananas@tut.by>
2019-08-11 20:24:02 +0300

    create after and before test

--- src/test/java/JDBCTest.java
+++ src/test/java/JDBCTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
@@ -10,25 +11,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class JDBCTest {
 
+    private  static Statement statement;
 
+    @BeforeAll
+    public static void before(){
+        statement=DatabaseService.getStatment();
+    }
 
     @AfterAll
-    public static void close()
+    public static void after()
     {
         DatabaseService.closeConnection(DatabaseService.getConnection());
     }
 
     @Test
     public void allOwnerMustBeHaveCar() throws SQLException {
-     Statement statement=DatabaseService.getStatment();
      ResultSet resultSet=statement.executeQuery("select id_owner from car");
      while(resultSet.next()){
       assertTrue(resultSet.getInt("id_owner")>0);
      }
-
     }
-
-
-
-
 }
