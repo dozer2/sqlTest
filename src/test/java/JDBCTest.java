7eeea7578f04529a5e723c3b64cc94d962a1374f
Sergei Aleinikov <5314955@gmail.com>
2019-08-16 23:58:17 +0300

    Two Tests

--- src/test/java/JDBCTest.java
+++ src/test/java/JDBCTest.java
@@ -26,10 +26,41 @@ public class JDBCTest {
 
     @Test
     public void allOwnerMustBeHaveCar() throws SQLException {
-     ResultSet resultSet=statement.executeQuery("select id_owner,name from car");
-     while(resultSet.next()){
-          String name= resultSet.getString("name");
-      assertTrue(resultSet.getInt("id_owner")>0,"Car with name ".concat(name).concat(" dont't have owner"));
-     }
+        ResultSet resultSet=statement.executeQuery("select id_owner,name from car");
+        while(resultSet.next()){
+            String name= resultSet.getString("name");
+            assertTrue(resultSet.getInt("id_owner")>0,"Car with name ".concat(name).concat(" dont't have owner"));
+        }
+    }
+
+    //Написать тест проверяющий есть ли машины у владельца
+    @Test
+    public void whetherTheOwnerHasCars() throws SQLException {
+        ResultSet resultSet = statement.executeQuery("SELECT owner.id, owner.name as owner, car.name\n" +
+                "FROM owner\n" +
+                "LEFT JOIN car ON owner.id = car.id_owner;");
+        while (resultSet.next()) {
+            String ownerWithoutCar = resultSet.getString("owner");
+            assertTrue(resultSet.getString("name") == null,
+                    "Owner with name ".concat(ownerWithoutCar).concat(" is have car"));
+
+        }
+    }
+
+    //Написать тест проверяющий есть ли владельцы с более чем двумя машинами
+    @Test
+    public void OwnersWithTwoOrMoreCars() throws SQLException {
+        ResultSet resultSet = statement.executeQuery("SELECT owner.id, owner.name as owner, count (car.name) as 'count cars'\n" +
+                "FROM owner\n" +
+                "LEFT JOIN car ON owner.id = car.id_owner\n" +
+                "GROUP by owner.id;");
+        while (resultSet.next()) {
+            String ownerWithTwoOrMoreCars = resultSet.getString("owner");
+            assertFalse(resultSet.getInt("count cars") > 1,
+                    "Owner with name ".concat(ownerWithTwoOrMoreCars).concat
+                            (" have ".concat(resultSet.getString("count cars").concat(" car"))));
+
+        }
     }
 }
+
